8ebfdefe6e1eba1c026865dbc4d7fa6bbd308adf
Abdullah Waqet <awaqet@Abdullah-Waqets-MacBook-Pro.local>
2017-02-14 00:36:53 +0300

    [new] init IntelMD-DEMO

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name intelMdDemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the intelMdDemoApp
+ */
+angular.module('intelMdDemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name intelMdDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the intelMdDemoApp
+ */
+angular.module('intelMdDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
