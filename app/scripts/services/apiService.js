307a0fa75ea644c5e095802c0dcc865e91c73fe0
Abdullah Waqet <awaqet@Abdullah-Waqets-MacBook-Pro.local>
2017-02-25 22:08:54 +0300

    [add] API Services

---
+++ app/scripts/services/apiService.js
@@ -0,0 +1,86 @@
+angular.module('Services')
+.factory('APIService', function($http, $rootScope) {
+	var api = {}
+	var domain = config.DOMAIN;
+	var requestServer = function (method, url, params, data) {
+		return $http({
+			method: method,
+			url: domain + url,
+			params: params,
+			data: data
+		});
+	}
+
+	api.authenticate = function (email, password) {
+		var url = "authenticate";
+		var params = {}
+		params.email = email;
+		params.password = password; //CryptoJS.SHA512(password).toString();
+		return requestServer('POST', url, params, {});
+	}
+	
+	api.generateNewObject = function(obj) {
+		var objClass = obj.getClass();
+		var url = objClass.url+'/new';
+		return requestServer('GET', url, {}, {})
+	}
+
+	api.saveNewObject = function(obj) {
+		var objClass = obj.getClass();
+		var url = objClass.url;
+		var data = {}
+		data[objClass.type] = obj;
+		return requestServer('POST', url, {}, data);
+	}
+
+	api.saveObject = function(obj) {
+		var objClass = obj.getClass();
+		var url = objClass.url+'/'+obj.id;
+		var data = {}
+		data[objClass.type] = obj;
+		return requestServer('PUT', url, {}, data);
+	}
+		
+	api.saveNewObjectWithOptions = function(obj,options) {
+		var url = options.url;
+		var data = {}
+		data[options.type] = obj;
+		return requestServer('POST', url, {}, data);
+	}
+
+	api.saveObjectWithOptions = function(obj,options) {
+		var url = options.url+'/'+obj.id;;
+		var data = {}
+		data[options.type] = obj;
+		return requestServer('PUT', url, {}, data);
+	}
+
+	api.destroyObject = function(obj) {
+		var objClass = obj.getClass();
+		var url = objClass.url+'/'+obj.id;
+		return requestServer('DELETE', url, {}, {});
+	}
+
+	api.destroyAll = function (objClass, ids) {
+		var url = objClass.url + '/' + 'destroy_all'
+		return requestServer('PUT', url, {}, ids);
+	}
+
+	api.getAll = function(objClass) {
+		var url = objClass.url;
+		return requestServer('GET', url, {}, {});
+	}
+
+	api.getAllWithParams = function(objClass, params) {
+		var url = objClass.url;
+		return requestServer('GET', url, params, {});
+	}
+
+	api.find = function(objClass, id) {
+		var url = objClass.url+'/'+id;
+		return requestServer('GET', url, {}, {});
+	}
+
+	
+	return api;
+});
